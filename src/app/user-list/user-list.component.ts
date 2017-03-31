c12bccc559e56587a06c4ec57cb1a5717e344462
Mateusz Stradałowicz <quantic92@gmail.com>
2017-03-31 16:58:17 +0200

    add user class and user mock table

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../user';
+
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
+
+  users: User[] = [
+    { id: 1, login: 'gabi89@o2.pl', name: 'Gabriel Hampton' },
+    { id: 2, login: 'moski_kiski@tlen.pl', name: 'Moses Massey' },
+    { id: 3, login: 'perl-harbor12@wp.pl', name: 'Pearl Ramirez' },
+    { id: 4, login: 'marek@marek.com', name: 'Mark Walsh' },
+    { id: 5, login: 'micz-z-mody-na-sukcez@aaa.pl', name: 'Mitchell Carter' }
+  ];
 
   constructor() { }
 
