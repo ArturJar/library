9a9071c11861e458fb09920c5d05974879b0587e
quantic92 <mati9.7.1992@wp.pl>
2017-04-09 22:18:15 +0200

    user module - cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { UserModule } from './user/user.module';
 
 import { AppComponent } from './app.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { AddUserComponent } from './add-user/add-user.component';
-
-import { UserService } from './user.service';
-import { UserDetailsComponent } from './user-details/user-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent,
-    AddUserComponent,
-    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    UserModule
   ],
-  providers: [UserService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
