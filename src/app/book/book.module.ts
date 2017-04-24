5098bea75c523193ec726eda142dfe30f9910f6d
Maciej Andrzejczuk <maciej.andrzejczuk@intive.com>
2017-04-24 17:22:58 +0200

    Add book module, service and book-list component

---
+++ src/app/book/book.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookListComponent } from './book-list/book-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    BookListComponent
+  ]
+})
+export class BookModule { }
