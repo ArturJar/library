f52871490829411caa22c80aa3b1633caa974b27
Artur Jarczewski <arturjarczewski@gmail.com>
2017-05-10 15:36:00 +0200

    test 3

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-//import { environment } from './environments/environment';  11
+//import { environment } from './environments/environment';  11123123
 if (environment.production) {
   enableProdMode();
 }
